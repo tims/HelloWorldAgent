0dd251452ea8a10da39ecf7a4d18de7b0aec899d
Tim Sell <trsell@gmail.com>
2011-02-24 14:51:12 +0000

    removed generated copy

--- src/main/java/com/sematext/HelloWorldAgent.java
+++ src/main/java/com/sematext/HelloWorldAgent.java
@@ -1,11 +1,3 @@
-/*
- *    Copyright (c) Sematext International
- *    All Rights Reserved
- *
- *    THIS IS UNPUBLISHED PROPRIETARY SOURCE CODE OF Sematext International
- *    The copyright notice above does not evidence any
- *    actual or intended publication of such source code.
- */
 package com.sematext;
 
 import java.lang.instrument.Instrumentation;
